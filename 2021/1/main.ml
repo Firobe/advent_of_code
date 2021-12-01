2d5b01c4610fa968a73f69219b597314d415ecdb
Virgile Robles <virgile.robles@protonmail.ch>
2021-12-01 11:45:39 +0100

    Day 1 Part 2

--- 2021/1/main.ml
+++ 2021/1/main.ml
@@ -1,4 +1,4 @@
-let solve l =
+let solve1 l =
   let r, _ = List.fold_left (fun (sum, last) current -> match last with
       | None -> (sum, Some current)
       | Some last ->
@@ -6,6 +6,21 @@ let solve l =
     ) (0, None) l
   in r
 
+(* Only compute sliding windows, and pass result to solve1 *)
+let solve2 l =
+  (* Current sum and last three elements *)
+  let rec aux sum (a, b, c) = function
+    | [] -> [sum]
+    | d :: t -> sum :: (aux (sum + d - a) (b, c, d) t)
+  in match l with
+  | a :: b :: c :: tail ->
+    aux (a + b + c) (a, b, c) tail |> solve1
+  | _ -> assert false
+
+
+let main file =
+  let input = Arg.read_arg file |> Array.to_list |> List.map int_of_string in
+  Printf.printf "%s\n========\nPart 1 %d\nPart 2 %d\n" file (solve1 input) (solve2 input)
+
 let () =
-  Arg.read_arg "input" |> Array.to_list
-  |> List.map int_of_string |> solve |> print_int
+  main "example" ; main "input"
