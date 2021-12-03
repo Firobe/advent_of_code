3ae062a9244c870d94e10cf0810457f42e8d1257
Virgile Robles <virgile.robles@protonmail.ch>
2021-12-03 10:47:04 +0100

    Day 3, done

---
+++ 2021/3/main.ml
@@ -0,0 +1,47 @@
+open Base
+
+let dec_of_bin l =
+  let chars = List.map ~f:(fun x -> if x then '1' else '0') l in
+  Int.of_string @@ "0b" ^ (String.of_char_list chars)
+
+let most_common_in_column pos l =
+  let t = List.transpose_exn l in
+  let len = List.length l in
+  let ones = List.nth_exn t pos |> List.count ~f:Fn.id in
+  if ones > len - ones then `One
+  else if ones = len - ones then `Equal
+  else `Zero
+
+let solve1 l =
+  let gamma = List.init (List.length (List.hd_exn l))
+      ~f:(fun pos -> match most_common_in_column pos l with
+          | `One -> true | `Zero -> false
+          | `Equal -> assert false) in
+  let epsilon = List.map ~f:(Bool.(<>) true) gamma in
+  (dec_of_bin gamma) * (dec_of_bin epsilon)
+
+let filter_by_bit b pos l =
+  List.filter l ~f:(fun l' -> Bool.(b = List.nth_exn l' pos))
+
+let rec filter_process criterion ?(pos=0) candidates = 
+  match candidates with
+  | [] -> failwith "No candidates left"
+  | [x] -> dec_of_bin x
+  | _ ->
+    let chosen = match criterion, most_common_in_column pos candidates with
+      | `Oxygen, (`One | `Equal) | `C02, `Zero -> true
+      | `Oxygen, `Zero | `C02, (`One | `Equal) -> false
+    in filter_process criterion ~pos:(pos + 1)
+      (filter_by_bit chosen pos candidates)
+
+let solve2 l =
+  (filter_process `Oxygen l) * (filter_process `C02 l)
+
+let convert_line l = l |> String.to_list |> List.map ~f:(Char.(=) '1')
+
+let main file =
+  let input = Stdio.In_channel.read_lines file |> List.map ~f:convert_line in
+  Stdio.printf "%s\n========\nPart 1 %d\nPart 2 %d\n" file (solve1 input) (solve2 input)
+
+let () =
+  main "example" ; main "input"
