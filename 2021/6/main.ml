2aa6ff8c36fdd115cd3a88cccdac5032031d4131
Virgile Robles <virgile.robles@protonmail.ch>
2021-12-06 20:23:08 +0100

    Day 6, use zarith instead of num

--- 2021/6/main.ml
+++ 2021/6/main.ml
@@ -1,30 +1,28 @@
 open Base
-open Num
 
 let one_day l =
   let zeroes = Queue.dequeue_exn l in
   Queue.enqueue l zeroes ;
-  Queue.set l 6 (Queue.get l 6 +/ zeroes) ;
+  Queue.set l 6 Z.(Queue.get l 6 + zeroes) ;
   l
 
 let rec n_days l n =
   if n = 0 then l
   else n_days (one_day l) (n - 1)
 
-let count_all l = Queue.fold l ~init:(Int 0) ~f:add_num
+let count_all l = Queue.fold l ~init:Z.zero ~f:Z.add
 
 let solve1 l = (n_days (Queue.copy l) 80) |> count_all
 let solve2 l = (n_days (Queue.copy l) 256) |> count_all
 
-let convert_data (l : string list) =
+let convert_data (l : string list) : Z.t Queue.t =
   let l = List.hd_exn l |> String.split ~on:',' |> List.map ~f:Int.of_string in
   List.init 9 ~f:(fun i -> List.count l ~f:((=) i))
-  |> List.map ~f:num_of_int |> Queue.of_list
+  |> List.map ~f:Z.of_int |> Queue.of_list
 
 let main file =
   let input = Stdio.In_channel.read_lines file |> convert_data in
-  Stdio.printf "%s\n========\nPart 1 %s\nPart 2 %s\n" file
-    (string_of_num (solve1 input))
-    (string_of_num (solve2 input))
+  Stdio.printf "%s\n========\nPart 1 %a\nPart 2 %a\n" file
+    Z.output (solve1 input) Z.output (solve2 input)
 
 let () = main "example" ; main "input"
