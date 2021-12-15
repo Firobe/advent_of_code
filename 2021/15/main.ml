9143622d464b78a6f7a5d734c5f892fdac370c86
Virgile Robles <virgile.robles@protonmail.ch>
2021-12-15 21:09:56 +0100

    Separate experiment

--- 2021/15/main.ml
+++ 2021/15/main.ml
@@ -1,28 +1,6 @@
 open Base
 
-module type CS = sig
-  type t
-  include Comparable.S with type t := t
-  include Sexpable.S with type t := t
-end
-
-(* just an experiment *)
-module Pair (A : CS) (B : CS) : CS with type t = A.t * B.t = struct
-  module O = struct
-    type t = A.t * B.t
-    let compare (a1,b1) (a2,b2) =
-      let c = A.compare a1 a2 in if c = 0 then B.compare b1 b2 else c
-    let sexp_of_t (a, b) = Sexp.List [A.sexp_of_t a; B.sexp_of_t b]
-    let t_of_sexp = function 
-      | Sexp.List [a; b] -> (A.t_of_sexp a, B.t_of_sexp b)
-      | _ -> assert false
-  end
-  include O
-  include Comparable.Make(O)
-  include Comparator.Make(O)
-end
-
-module PQ = Psq.Make(Pair(Int)(Int))(Int)
+module PQ = Psq.Make(Pair.Make(Int)(Int))(Int)
 
 let shortest_path size (risk : int * int -> int) src dst =
   let prio = PQ.sg src 0 in
