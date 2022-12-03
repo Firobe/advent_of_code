be240fcbaf71ac5b1d56045da933a3e077a6bc65
Virgile Robles <virgile.robles@protonmail.ch>
2022-12-03 13:56:56 +0100

    Day 3

---
+++ 2022/03/aoc.ml
@@ -0,0 +1,66 @@
+open Aoclib
+
+module CBag = Bag.Make (struct 
+    type t = char
+    let compare = Char.compare
+  end)
+
+module Types = struct
+  type bag = {left: CBag.t; right: CBag.t}
+  type input = bag list
+
+  type output = int
+  [@@deriving show]
+end
+include Types
+
+module Parsing = struct
+  open Angstrom
+
+  let item = satisfy (function 'a'..'z' | 'A'..'Z' -> true | _ -> false)
+
+  let create_bag str =
+    let f bag c = CBag.add c bag in
+    String.fold_left f CBag.empty str
+
+  let create_bags str =
+    let cutoff = (String.length str) / 2 in
+    let left = String.sub str 0 cutoff |> create_bag in
+    let right = String.sub str cutoff cutoff |> create_bag in
+    {left; right}
+
+  let bag = (consumed (many1 item) >>| create_bags) <* end_of_line
+
+  let input = many bag
+end
+
+module Solving = struct
+  open Base
+
+  let priority c = match c with
+    | 'a'..'z' -> Char.to_int c - Char.to_int 'a' + 1
+    | 'A'..'Z' -> Char.to_int c - Char.to_int 'A' + 27
+    | _ -> assert false
+
+  let merge_bag b = CBag.union b.left b.right
+  let prio_singleton b = CBag.choose b |> fst |> priority
+
+  let part1 (input : input) : output =
+    input
+    |> List.map ~f:(fun bag -> CBag.inter bag.left bag.right)
+    |> List.sum (module Int) ~f:prio_singleton
+
+  let part2 (input : input) : output =
+    input
+    |> List.chunks_of ~length:3
+    |> List.map ~f:(function
+        |[a; b; c] ->
+          CBag.(inter (inter (merge_bag a) (merge_bag b)) (merge_bag c))
+        | _ -> assert false
+      )
+    |> List.sum (module Int) ~f:prio_singleton
+end
+
+module Today = MakeDay(Types)(Parsing)(Solving)
+
+let () = Today.run_all
