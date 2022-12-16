ae2555b7219e203af3215a1dd441ea64a7ef2a86
Virgile Robles <virgile.robles@protonmail.ch>
2022-12-16 16:20:45 +0100

    2022 day 16

--- 2022/16/aoc.ml
+++ 2022/16/aoc.ml
@@ -46,29 +46,84 @@ module Solving = struct
     |> GJ.all_pairs_shortest_paths
 
   let interesting_nodes input =
-    List.filter_map input ~f:(fun (name, { flow; _ }) ->
-        if flow > 0 then Some (name, flow) else None)
+    let flows = Hashtbl.create (module String) in
+    let set =
+      List.filter_map input ~f:(fun (name, { flow; _ }) ->
+          if flow > 0 then (
+            Hashtbl.add_exn flows ~key:name ~data:flow;
+            Some name)
+          else None)
+      |> Set.of_list (module String)
+    in
+    (set, flows)
 
   let search input =
     let distances = all_distances input in
+    let valves, flows = interesting_nodes input in
     let rec aux time_left valves_left score pos =
       if time_left < 0 then score
       else
-        List.map valves_left ~f:(fun (next, flow) ->
+        Set.map
+          (module Int)
+          valves_left
+          ~f:(fun next ->
             let time = time_left - GJ.HVV.find distances (pos, next) - 1 in
-            let score = score + max 0 (time * flow) in
-            let left =
-              List.filter valves_left ~f:(fun (s, _) ->
-                  not (String.equal s next))
-            in
+            let score = score + max 0 (time * Hashtbl.find_exn flows next) in
+            let left = Set.remove valves_left next in
             aux time left score next)
-        |> List.max_elt ~compare
+        |> Set.max_elt
         |> Option.value ~default:score
     in
-    aux 30 (interesting_nodes input) 0 "AA"
+    aux 30 valves 0 "AA"
+
+  let search2 input =
+    let distances = all_distances input in
+    let valves, flows = interesting_nodes input in
+    let rec aux time_left valves_left score (pos1, pos2) =
+      let go_next pos mode =
+        Set.filter_map
+          (module Int)
+          valves_left
+          ~f:(fun next ->
+            let dist = GJ.HVV.find distances (pos, next) + 1 in
+            let time = time_left - dist in
+            if time < 0 then None
+            else
+              let score = score + max 0 (time * Hashtbl.find_exn flows next) in
+              let left = Set.remove valves_left next in
+              let pos = `Moving (next, dist) in
+              let next_pos =
+                match mode with `Left -> (pos, pos2) | `Right -> (pos1, pos)
+              in
+              Some (aux time_left left score next_pos))
+        |> Set.max_elt
+        |> Option.value_or_thunk ~default:(fun () ->
+               match mode with
+               | `Left -> aux time_left valves_left score (`Stuck, pos2)
+               | `Right -> aux time_left valves_left score (pos1, `Stuck))
+      in
+      if time_left < 0 then score
+      else
+        match (pos1, pos2) with
+        | `Moving (n1, d1), `Moving (n2, d2) ->
+            let pos1, pos2, delta =
+              if d1 = d2 then (`At n1, `At n2, d1)
+              else if d1 > d2 then (`Moving (n1, d1 - d2), `At n2, d2)
+              else (`At n1, `Moving (n2, d2 - d1), d1)
+            in
+            aux (time_left - delta) valves_left score (pos1, pos2)
+        | `Stuck, `Moving (n, d) ->
+            aux (time_left - d) valves_left score (pos1, `At n)
+        | `Moving (n, d), `Stuck ->
+            aux (time_left - d) valves_left score (`At n, pos2)
+        | `At pos, _ -> go_next pos `Left
+        | _, `At pos -> go_next pos `Right
+        | `Stuck, `Stuck -> score
+    in
+    aux 26 valves 0 (`At "AA", `At "AA")
 
   let part1 (input : input) : output = search input
-  let part2 (input : input) : output = List.length input
+  let part2 (input : input) : output = search2 input
 end
 
 module Today = MakeDay (Types) (Parsing) (Solving)
